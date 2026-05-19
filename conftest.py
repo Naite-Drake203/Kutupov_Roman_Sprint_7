faf6c46d3950763fab831bb3dc866bbc76906638
Naite-Drake203 <162971506+Naite-Drake203@users.noreply.github.com>
2026-05-19 14:57:04 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,7 +1,5 @@
 import pytest
-
 from helps import Courier
-
 
 # фикстура регистрации, авторизации и удаления курьера
 @pytest.fixture()
@@ -15,6 +13,5 @@ def courier():
 @pytest.fixture()
 def courier_delete():
     courier_create = Courier().courier_registration_in_the_system_and_get_courier_data()
-    print(courier_create['data'])
     courier_login = Courier().courier_login_in_the_system_and_get_id_courier(courier_create["data"])
     yield courier_login
